5ce8ac6f4ea551e4f79981f81ae3ab292be3b717
Szalay Tamás <szalaytamas90@gmail.com>
2021-12-12 15:45:40 +0100

    init

---
+++ src/main/java/aquarium/UniqueFinder.java
@@ -0,0 +1,9 @@
+package aquarium;
+
+import java.util.List;
+
+public class UniqueFinder {
+    public List<Character> getUniqueChars(String word){
+        List<Character> result;
+    return result;}
+}
